a65b91e07cd7f02b4f3c60c1ddb999aedc31ac6b
Swapnil Markhedkar <Swapnil.sam.13@gmail.com>
2018-12-11 22:39:14 +0530

    Init Commit

---
+++ server/models/todo.js
@@ -0,0 +1,45 @@
+var mongoose = require('mongoose');
+
+var Todo = mongoose.model('Todo', {
+	text: {
+		type :String,
+		required :true,
+		minlength:1,
+		trim :true
+	},
+	completed :{
+		type:Boolean,
+		default :false
+	},
+	completedAt:{
+		type:Number,
+		default :null
+	}
+});
+
+module.exports = {Todo};
+
+
+// var newTodo = new Todo({
+// 	text : 'Cook Alastair',
+// 	completed:true,
+// 	completedAt:78
+// });
+
+// newTodo.save().then((doc) =>{
+// 	console.log('Saved Todo:', doc);
+// }, (err) => {
+// 	console.log('Unable to save Todo');
+// });
+
+// var newTodo2 = new Todo({
+// 	text: 'Study DSA',
+// 	completed: false,
+// 	completedAt: 1
+// });
+
+// newTodo2.save().then((doc) => {
+// 	console.log(`Success : ${doc}`);
+// }, (err) =>{
+// 	console.log(`Failed! : ${err}`);
+// });
