1e7b3ca2640c3b2a0637cc72c6cad17a03417fdd
Swapnil Markhedkar <Swapnil.sam.13@gmail.com>
2018-12-18 00:29:22 +0530

    Add Patch/todos/:id

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ var {User}= require('./models/user');
 var express = require('express');
 var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
+var _ = require('lodash');
 
 var app= express();
 var port = process.env.PORT || 3000;
@@ -65,13 +66,41 @@ app.delete('/todos/:id', (req,res) => {
 			console.log('No Todo');
 		}
 		
-		res.send(todo);
+		res.send({todo});
 		console.log('Todo Deleted');
 	}).catch((e) => {
 		res.status(400).send();
 		console.log('Error Caught');
 	});
 });
+
+app.patch('/todos/:id', (req,res) => {
+	var id = req.params.id;
+	var body = _.pick(req.body, ['text', 'completed']);
+	
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+		console.log('Not Valid');
+	}	
+
+	if(_.isBoolean(body.completed) &&body.completed){
+		body.completedAt=new Date().getTime();
+	}
+	else{
+		body.completed=false;
+		body.completedAt=null;
+	}
+
+	Todo.findByIdAndUpdate(id, {$set: body}, {new:true}).then((todo) => {
+		if(!todo)
+			return res.status(404).send();
+	
+		res.send({todo});
+	}).catch((e) => {
+		res.status(400).send();
+	});
+});
+
 app.listen(port, ()=>{
 	console.log(`Server Up and Running on ${port}`);
 });
